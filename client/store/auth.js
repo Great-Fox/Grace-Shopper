3481103ad6f1816472f2331038b433d401c3d6cf
wisch628 <hannahwischnia@gmail.com>
2021-04-21 13:33:05 -0600

    deleted a console log

--- client/store/auth.js
+++ client/store/auth.js
@@ -19,7 +19,6 @@ const setAuth = (auth) => ({ type: SET_AUTH, auth });
 export const me = () => async (dispatch) => {
   const token = window.localStorage.getItem(TOKEN);
   if (token !== 'undefined' && token) {
-    console.log(token);
     const res = await axios.get('/auth/me', {
       headers: {
         authorization: token,
