aa7d1c848876b9a98a1966c55a16a89dad6a1ac1
Anna Strasser <albastrasser@gmail.com>
2021-04-22 12:34:54 -0400

    with backend routes for editing deleting and adding ringtones and admin security

--- server/api/admin.js
+++ server/api/admin.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {
-  models: { User, Order, Order_Ringtone, Ringtone },
+  models: { User, Ringtone },
 } = require('../db');
 const { requireToken, isAdmin } = require('./gatekeepingMiddleware');
 
@@ -9,6 +9,39 @@ router.get('/users', requireToken, isAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll();
     res.json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//POST /admin/ringtones
+router.post('/ringtones', requireToken, isAdmin, async (req, res, next) => {
+  try {
+    let newRingtone = await Ringtone.create(req.body);
+    res.json(newRingtone);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//PUT /admin/:ringtoneId
+router.put('/:ringtoneId', requireToken, isAdmin, async (req, res, next) => {
+  try {
+    let ringtone = await Ringtone.findByPk(req.params.ringtoneId);
+    let updatedRingtone = await ringtone.update(req.body);
+    res.json(updatedRingtone);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//DELETE /admin/:ringtoneId
+
+router.delete('/:ringtoneId', requireToken, isAdmin, async (req, res, next) => {
+  try {
+    let ringtone = await Ringtone.findByPk(req.params.ringtoneId);
+    await ringtone.destroy();
+    res.json(ringtone);
   } catch (error) {
     next(error);
   }
