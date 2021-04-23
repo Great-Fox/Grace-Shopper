aadcd278749e2f36d26d06d84f1c98bfa18bd8fc
Anna Strasser <albastrasser@gmail.com>
2021-04-23 14:47:56 -0400

    model updated

--- server/db/models/ringtone.js
+++ server/db/models/ringtone.js
@@ -1,31 +1,31 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require('sequelize');
+const db = require('../db');
 
 const Ringtone = db.define('ringtone', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-        }
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
     },
-    artist: {
-        type: Sequelize.STRING
+  },
+  artist: {
+    type: Sequelize.STRING,
+  },
+  genre: {
+    type: Sequelize.STRING,
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
     },
-    genre: {
-        type: Sequelize.STRING
-    },
-    price : {
-        type: Sequelize.DOUBLE,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-        },
-        defaultValue: 1.99
-    },
-    songUrl: {
-        type: Sequelize.TEXT
-    }
-})
+    defaultValue: 199,
+  },
+  songUrl: {
+    type: Sequelize.TEXT,
+  },
+});
 
-module.exports = Ringtone
+module.exports = Ringtone;
