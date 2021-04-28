a630c1390df4c3c700360f3d4193c6fd30a3be68
Anna Strasser <albastrasser@gmail.com>
2021-04-28 07:56:05 -0400

    order total divide by 100

--- client/components/AccountInfo/OrderHistory.js
+++ client/components/AccountInfo/OrderHistory.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getAllOrderHistoryThunk } from '../../store/redux/orderHistory'
+import { getAllOrderHistoryThunk } from '../../store/redux/orderHistory';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -14,62 +14,69 @@ const columns = [
 ];
 
 export class OrderHistory extends React.Component {
+  componentDidMount() {
+    this.props.getOrders(this.props.match.params.userId);
+  }
 
- componentDidMount() {
-    this.props.getOrders(this.props.match.params.userId)
-}
-
-  render(){
-      const orders = this.props.orders
-      return (
-        <div>
-        <Paper margin='auto' maxWidth={500} >
-            <Typography gutterBottom variant="h3" style={{ marginTop: 5 }}>
-                Your Orders History 
-                </Typography>
-            <Box display="flex" justifyContent="center">
+  render() {
+    const orders = this.props.orders;
+    return (
+      <div>
+        <Paper margin="auto" maxWidth={500}>
+          <Typography gutterBottom variant="h3" style={{ marginTop: 5 }}>
+            Your Orders History
+          </Typography>
+          <Box display="flex" justifyContent="center">
             <Grid container spacing={12}>
-                {orders.map(order => {
-                    return (
-                      <Grid item xs={12} sm container style={{ margin: 5 }}>
-                      <Paper style={{ padding: 100 }}>
-
+              {orders.map((order) => {
+                return (
+                  <Grid item xs={12} sm container style={{ margin: 5 }}>
+                    <Paper style={{ padding: 100 }}>
                       <Grid item xs container direction="column" spacing={2}>
                         <Grid item xs>
-                        <Typography gutterBottom variant="h4">
-                          Order {order.id}
-                        </Typography>
-                              <div style={{ display: 'flex', height: 300, background: "white"}}>
-                              <div style={{ flexGrow: 1}}>
-                                <DataGrid columns={columns} rows={order.ringtones} />
-                              </div>
+                          <Typography gutterBottom variant="h4">
+                            Order {order.id}
+                          </Typography>
+                          <div
+                            style={{
+                              display: 'flex',
+                              height: 300,
+                              background: 'white',
+                            }}>
+                            <div style={{ flexGrow: 1 }}>
+                              <DataGrid
+                                columns={columns}
+                                rows={order.ringtones}
+                              />
                             </div>
-                          </Grid>
+                          </div>
                         </Grid>
-                        <Grid item>
-                          <Typography variant="h4" style={{ marginTop: 10 }}>TOTAL PRICE: ${order.totalPrice}</Typography>    
-                        </Grid>
-
-                      </Paper>
                       </Grid>
-                    )
-                })}
-              </Grid>
-              </Box>
-          </Paper>
-        </div>
-      )
+                      <Grid item>
+                        <Typography variant="h4" style={{ marginTop: 10 }}>
+                          TOTAL PRICE: ${order.totalPrice / 100}
+                        </Typography>
+                      </Grid>
+                    </Paper>
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </Box>
+        </Paper>
+      </div>
+    );
   }
 }
-  const mapState = (state) => ({
-    orders: state.orders,
-    userId: state.auth.id,
-  });
+const mapState = (state) => ({
+  orders: state.orders,
+  userId: state.auth.id,
+});
 
-  const mapDispatch = (dispatch) => {
-    return {
-        getOrders: (userId) => dispatch(getAllOrderHistoryThunk(userId))
-    };
+const mapDispatch = (dispatch) => {
+  return {
+    getOrders: (userId) => dispatch(getAllOrderHistoryThunk(userId)),
   };
-  
-  export default connect(mapState, mapDispatch)(OrderHistory);
+};
+
+export default connect(mapState, mapDispatch)(OrderHistory);
